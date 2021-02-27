393cf4b6d165c029a7bd7c70e08873659d492e5e
Kotth <arnaud.nilens@gmail.com>
2021-02-27 12:46:07 +0100

    update V1.1

--- portfolio/src/app/profile/about/about.component.ts
+++ portfolio/src/app/profile/about/about.component.ts
@@ -8,12 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() {
-    this.age = new Date().getFullYear() - 1998 + ' ans';
+    this.age = this.calculateAge();
   }
 
   age: string;
 
   ngOnInit(): void {
+
+  }
+
+  calculateAge():string {
+    const birthDate = new Date(1998, 1, 13);
+    var diff_ms = Date.now() - birthDate.getTime();
+    var age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970) + ' ans';
   }
 
 
